62dc2b1e395a7087e680243058dfd876826a684d
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-18 15:44:28 -0800

    pre code review

---
+++ src/Pokecard.js
@@ -0,0 +1,14 @@
+function Pokecard({id, name, type, base_experience}){
+  return (
+    <div className="card text-white bg-secondary m-1 d-inline-block">
+      <div className="card-header"><b>{name}</b></div>
+      <div className="card-body">
+        <img alt="pokemon {name}" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}></img>
+        <h4>Type: {type}</h4>
+        <p className="card-text">Base EXP: {base_experience}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Pokecard;
