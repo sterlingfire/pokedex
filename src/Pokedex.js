8e67ccaa3efef24eaac4751d037778199161b4ba
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-18 16:53:05 -0800

    pokegame

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,15 +1,19 @@
 import Pokecard from "./Pokecard";
 
 /* Pokedex component for pokedex app
- * props = Pokemon (array of pokemon objects)
+ * props = Pokemon (an array of pokemon objects), totalExp, isWinner
  ** pokemon objects look like: {id:int, name:string, type:string, base_experience:int}
+ * totalExp:int (total base_experience for all pokemon in hand)
+ * isWinner:boolean (Did this hand win?)
  * displays each pokemon as a bootstrap card (using the pokecard component)
  */
-function Pokedex({pokemon}){
+function Pokedex({pokemon, totalExp, isWinner}){
   console.log('pokemon in Pokedex', pokemon);
   return (
     <div className="Pokedex">
       { pokemon.map(mon => Pokecard(mon)) }
+      <h3>{isWinner? "This hand won!":""}</h3>
+      <p>Total Experience: {totalExp}</p>
     </div>
   )
 
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,8 +1,8 @@
 import _ from "lodash";
 import Pokedex from "./Pokedex";
 
-/** Pokegame component that renders two Pokedex "hands" 
- * 
+/** Pokegame component that renders two Pokedex "hands"
+ *
  * Props: props = Pokemon (array of pokemon objects)
  * pokemon objects look like: {id:int, name:string, type:string, base_experience:int}
  */
@@ -10,12 +10,23 @@ import Pokedex from "./Pokedex";
 function Pokegame({pokemon}) {
   let shuffledPokemon = _.shuffle(pokemon);
   console.log('shuffled pokemon', shuffledPokemon);
+  let hand1 = shuffledPokemon.slice(0,4);
+  let hand2 = shuffledPokemon.slice(4);
+  let hand1Total = hand1.reduce((a, c) => a+=c.base_experience,0);
+  let hand2Total = hand2.reduce((a, c) => a+=c.base_experience,0);
+
   return (
     <div className="Pokegame">
       <h3>Hand one</h3>
-      <Pokedex pokemon={shuffledPokemon.slice(0,4)}/>
+      <Pokedex
+        pokemon={hand1}
+        totalExp={hand1Total}
+        isWinner={hand1Total>hand2Total ? true : false} />
       <h3>Hand two</h3>
-      <Pokedex pokemon={shuffledPokemon.slice(4)}/>
+      <Pokedex
+        pokemon={hand2}
+        totalExp={hand2Total}
+        isWinner={hand2Total>hand1Total ? true : false} />
     </div>
   )
 }
@@ -29,8 +40,7 @@ const POKEMON = [
   {id: 94,  name: 'Gengar',     type: 'poison',   base_experience: 225},
   {id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65}
   ];
-  
+
 Pokegame.defaultProps = {pokemon:POKEMON};
 
 export default Pokegame;
-  
